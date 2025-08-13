07ec436d0e5b81f580c0ab37833b53f3099b49ef
Karan M <karanmohokar7@gmail.com>
2025-08-13 17:12:55 +0530

    first commit

---
+++ src/main/java/com/anuj/redditmessagemanager/RedditMessageManagerApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.redditmessagemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditMessageManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditMessageManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuj/redditmessagemanager/RedditMessageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.redditmessagemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditMessageManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
